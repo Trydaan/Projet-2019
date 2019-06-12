38a6536bdf3df7f73ac73d0a929b5654e02abc15
Baptiste <baptiste.lequeu@gmail.com>
2019-06-12 13:19:25 +0200

    Revert "Premier commit"
    This reverts commit f0b2993b6c3b52c51aff79db81d6435ecfdcf47d.

---
+++ AboMag/AboMag/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AboMag.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Nom { get; set; }
+
+        public string Prenom { get; set; }
+
+        public string Mail { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateNaissance { get; set; }
+
+        public string LieuNaissance { get; set; }
+
+        public string Password { get; set; }
+
+
+    }
+}
---
+++ AboMag/AboMag/Pages/Users/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AboMag.Models;
+
+namespace AboMag.Pages.Users
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly AboMag.Models.AboMagContext _context;
+
+        public DetailsModel(AboMag.Models.AboMagContext context)
+        {
+            _context = context;
+        }
+
+        public User User { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            User = await _context.User.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (User == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
